403a1c4ec262f656e7ae2038000e944ca099184f
Richard Stelling <richard.stelling@vistair.com>
2013-12-13 15:47:24 +0000

    - Added stub files for file based hashing category

---
+++ NSURL+Hashing.h
@@ -0,0 +1,13 @@
+//
+//  NSURL+Hashing.h
+//  simple-foundation-hashing
+//
+//  Created by Richard Stelling on 13/12/2013.
+//  Copyright (c) 2013 Empirical Magic Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (Hashing)
+
+@end
---
+++ NSURL+Hashing.m
@@ -0,0 +1,13 @@
+//
+//  NSURL+Hashing.m
+//  simple-foundation-hashing
+//
+//  Created by Richard Stelling on 13/12/2013.
+//  Copyright (c) 2013 Empirical Magic Ltd. All rights reserved.
+//
+
+#import "NSURL+Hashing.h"
+
+@implementation NSURL (Hashing)
+
+@end
