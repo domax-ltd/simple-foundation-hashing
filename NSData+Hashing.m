40ff4ac506e9415679325cc9742262f7cc0d165c
Richard Stelling <richard.stelling@vistair.com>
2013-12-23 14:40:48 +0000

    - Added stubs for NSData+Hashing. Initial version will support CRC on small NSData only.

---
+++ NSData+Hashing.m
@@ -0,0 +1,13 @@
+//
+//  NSData+Hashing.m
+//  simple-foundation-hashing
+//
+//  Created by Richard Stelling on 23/12/2013.
+//  Copyright (c) 2013  Empirical Magic Ltd. All rights reserved.
+//
+
+#import "NSData+Hashing.h"
+
+@implementation NSData (Hashing)
+
+@end
